68cb266c8ce3e2a9dae8028c142a5199466985a8
breno.martins <breno.martins@hubconexa>
2024-03-04 20:59:45 -0300

    Update e2e_Register.cy.js

--- e2e/cypress/integration/e2e/userAuth/e2e_Register.cy.js
+++ e2e/cypress/integration/e2e/userAuth/e2e_Register.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
 import '@shelex/cypress-allure-plugin';
-import '../../../support/e2e_commands/user_auth/register_commands'
 import testDescription from "./bdd-description/userAuth.description";
 import register from "../../../support/e2e_commands/user_auth/register";
 
